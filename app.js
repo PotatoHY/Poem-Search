106e76ae5f23748da17af0963602d7e4a8b0d19f
Sola <kylesora419@gmail.com>
2021-11-26 00:56:57 +0800

    when sleep

--- app.js
+++ app.js
@@ -3,42 +3,39 @@ var poemList = [
     title: "靜夜思", 
     author: "李白", 
     content: "床前明月光，疑是地上霜。舉頭望明月，低頭思故鄉。", 
-    keywords: [] 
+    keywords: ["光","月"] 
   },
   { 
     title: "涼州詞", 
     author: "王翰", 
     content: "葡萄美酒夜光杯，欲飲琵琶馬上催。醉臥沙場君莫笑，古來征戰幾人回！", 
-    keywords: [] 
+    keywords: ["光","酒","人"] 
   },
   { 
     title: "念奴嬌·赤壁懷古", 
     author: "蘇軾", 
-    content: "大江東去，浪淘盡，千古風流人物。故壘西邊，人道是，三國周郎赤壁。亂石穿空，驚濤拍岸，捲起千堆雪。江山如畫，一時多少豪傑。遙想公瑾當年，小喬初嫁了，雄姿英發。羽扇綸巾，談笑間，檣櫓灰飛煙滅。故國神遊，多情應笑我，早生華髮。人生如夢，一尊還酹江月。(人生 一作：人間；尊 通：樽)", 
-    keywords: [] 
-  }
+    content: "大江東去，浪淘盡，千古風流人物。故壘西邊，人道是，三國周郎赤壁。亂石穿空，驚濤拍岸，捲起千堆雪。江山如畫，一時多少豪傑。遙想公瑾當年，小喬初嫁了，雄姿英發。羽扇綸巾，談笑間，檣櫓灰飛煙滅。故國神遊，多情應笑我，早生華髮。人生如夢，一尊還酹江。", 
+    keywords: ["月","人"] 
+  },
+  {
+    title: "甜蜜的復仇",
+    author: "夏宇",
+    content: "把你的影子加點鹽 醃起來 風乾 老的時候 下酒 ",
+    keyword: ["酒"]
+  },
 ]
 
-function search(event) {
+function search(event, key) {
   var resultsDiv = document.getElementById("results")
 
   // TODO: get rid of else statement after debugging
-  if (event)
-    var word = event.srcElement.innerHTML
-  else {
-    // var inputField = document.getElementById("word")
-    // if (inputField)
-    //   var word = inputField.value
-  }
 
-  if (word === "") {
-    resultsDiv.innerHTML = ""
-    return
-  }
+  var word = key;
 
   // search logic
   var results = []
   poemList.forEach((poem) => {
+    console.log(word)
     if (poem.content.indexOf(word) >= 0) {
       results.push(poem)
     }
@@ -51,16 +48,13 @@ function search(event) {
   text.innerHTML = "相關詩詞："
   text.setAttribute("id", "相關詩詞")
   resultsDiv.append(text)
-
   results.forEach((poem) => {
     // TODO: 記得skip自己
-    title = document.createElement("h2")
-    title.innerHTML = poem.title
-    author = document.createElement("h3")
-    author.innerHTML = poem.author
-    content = document.createElement("p")
-    content.innerHTML = poem.content
-    resultsDiv.append(title, author, content)
+    title = document.createElement("p")
+    title.innerHTML = '<label style="color: red" onclick="onClickTitle(event)">' + poem.title + '</label>'
+    resultsDiv.append(title)
+      
+
   })
 }
 
@@ -80,6 +74,10 @@ function onClickTitle(event){
   author.innerHTML = requiredPoem.author
   content = document.createElement("p")
   content.innerHTML = requiredPoem.content
+  requiredPoem.keywords.forEach((key)=>{
+        content.innerHTML = content.innerHTML.replace(key, '<label style="color: blue" onclick="search(event,\''+ key + '\')">' + key + '</label>')
+    })
+
 
   // TODO: change keywords in content to label onClick = "search(event)"
   // TODO: change label CSS
